fbbaaa75224c1eb8e3df836db1ff7a4042701165
Niko Draca <me@nikodraca.com>
2020-03-09 16:30:01 -0400

    Fix vis.js missing null image bug

--- src/reducers/storeSpotifyResponse.reducer.js
+++ src/reducers/storeSpotifyResponse.reducer.js
@@ -6,6 +6,11 @@ export default (state, actionPayload) => {
     state = state.set('artistGraph', Immutable.fromJS(null));
     state = state.set('user', Immutable.fromJS(null));
   } else {
+    // handle error with vis.js and null images
+    actionPayload.artistGraph.nodes = actionPayload.artistGraph.nodes.map(node => {
+      return { ...node, image: node.image || '' };
+    });
+
     state = state.set('artistGraph', Immutable.fromJS(actionPayload.artistGraph));
     state = state.set('user', Immutable.fromJS(actionPayload.user));
   }
